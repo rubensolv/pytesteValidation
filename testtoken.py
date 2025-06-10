61426e7020ba612fe70397ec62fc6981af72ba1a
Rubens de Oliveira Moraes Filho <rubens_olv@hotmail.com>
2025-06-10 15:51:51 -0300

    Update testtoken.py

--- testtoken.py
+++ testtoken.py
@@ -1,13 +1,14 @@
 import os
-AUTH_TOKEN = os.environ.get("AUTH_TOKEN_JAMES_OFICIAL2")#os.environ['AUTH_TOKEN_JAMES_OFICIAL']
-if not AUTH_TOKEN:
-    raise ValueError("AUTH_TOKEN_JAMES_OFICIAL2 environment variable is not set.")
-
-print(AUTH_TOKEN)
-
 
 AUTH_TOKEN = os.environ.get("AUTH_TOKEN_JAMES_OFICIAL")#os.environ['AUTH_TOKEN_JAMES_OFICIAL']
 if not AUTH_TOKEN:
     raise ValueError("AUTH_TOKEN_JAMES_OFICIAL environment variable is not set.")
+
+print(AUTH_TOKEN)
+
+
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN_JAMES_OFICIAL2")#os.environ['AUTH_TOKEN_JAMES_OFICIAL']
+if not AUTH_TOKEN:
+    raise ValueError("AUTH_TOKEN_JAMES_OFICIAL2 environment variable is not set.")
 
 print(AUTH_TOKEN)
